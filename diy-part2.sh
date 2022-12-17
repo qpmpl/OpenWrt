1ad3075bb54e8dbd1c5a39bedd88c3ea61b65739
qpmpl <58429446+qpmpl@users.noreply.github.com>
2022-12-17 17:44:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,20 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.251/g' package/base-files/files/bin/config_generate
 
-# clone openclash
+# Clone openclash
 mkdir package/luci-app-openclash
 cd package/luci-app-openclash
 git init
 git remote add -f origin https://github.com/vernesong/OpenClash.git
 git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
+git pull origin master
 git branch --set-upstream-to=origin/master master
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
 # 回退到主项目目录
 cd ../..
