63d0027b85dab5d49ae932bb4e01294b91c8b0f8
qpmpl <58429446+qpmpl@users.noreply.github.com>
2024-03-23 23:20:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,4 @@ echo "src-git helloworld  https://github.com/fw876/helloworld.git" >> "feeds.con
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git homeproxy https://github.com/kenzok8/small-package/tree/main/homeproxy' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
